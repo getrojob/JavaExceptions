ed17b89038149cd25b62b503678fb2c85f0ee74a
getrojob <getrojob@gmai.com>
2023-07-09 13:17:24 -0300

    add project fail design

---
+++ src/test/java/com/trataerror/trataerror/TrataerrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trataerror.trataerror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrataerrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
